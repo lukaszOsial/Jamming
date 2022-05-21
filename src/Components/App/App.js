1c8b7f363b5878e7beb985971430dbb07375a8e2
lukaszOsial <lukasz.osial@gmail.comq$git config --global user.email lukasz.osial@gmail.com>
2022-05-21 18:42:41 +0200

    new project

---
+++ src/Components/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import SearchResults from '../SearchResults/SearchResults';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state.searchResults = [];
+  }
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+            //Add a SearchBar component
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            //Add a Playlist component
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
